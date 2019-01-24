f72ddd8f6217f6d5a93835caad8e7703e7754314
Raja Prahara <raja.ardri@gmail.com>
2019-01-24 11:23:53 +0100

    all images are moving as rule of the game

--- js/script.js
+++ js/script.js
@@ -7,103 +7,151 @@ var canvas = document.querySelector(".baby-floor");
 // context object to draw things
 var ctx = canvas.getContext("2d");
 
-// Create Characters (Baby, Diaper, Bottle of Milk, handwash)
+// Create Characters (Parent, Baby, Diaper, Bottle of Milk, handwash)
+
+class Parent {
+  constructor(img, x) {
+    this.img = img;
+    this.x = x;
+    this.y = 70;
+    this.width = 90;
+    this.height = 90;
+  }
+
+  drawParent() {
+    this.x += 1;
+
+    // if the x is reaching the canvas width
+    if (this.x == canvas.width - 90) {
+      // reset to return OPTION width
+      this.x = 0;
+    }
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+}
+
+// ---------------Parent------------------
+var parentImg = new Image();
+parentImg.src = "./img/ParentTransparent.png";
+
+function drawParent() {
+  ctx.drawImage(parentImg, parentX, parentY, 90, 70);
+}
+// var parentX = 200;
+// var parentY = 100;
+var newParent = new Parent(parentImg, 70);
+
+// ------------baby----------------------
 var babyImg = new Image();
 babyImg.src = "./img/happybaby.png";
 
-babyImg.onload = function() {
-  drawBaby();
-  drawHand();
-  drawDiaper();
-  drawMilk();
-};
-
 function drawBaby() {
-  ctx.drawImage(babyImg, babyX, babyY, 50, 50);
+  ctx.drawImage(babyImg, babyX, babyY, 30, 30);
 }
 
-var babyX = 0;
-var babyY = 250;
+var babyX = 400;
+var babyY = 100;
 
-function move(dir) {
-  myGamePiece.image.src = "angry.gif";
-  if (dir == "up") {
-    myGamePiece.speedY = -1;
+// --------------baby fall -- gravity -----------------
+// class BabyFall {
+//   constructor() {
+//     this.img = img;
+//     this.width = width;
+//     this.height = height;
+//     this.x = 0;
+//     this.y = 500;
+//     this.speedX = 0;
+//     this.speedY = 0;
+//     this.gravity = 0.05;
+//     this.gravitySpeed = 0;
+//     this.update = function() {
+//       ctx = canvas.context;
+//       ctx.fillRect(this.x, this.y, this.width, this.height);
+//     };
+//     this.newPos = function() {
+//       this.gravitySpeed += this.gravity;
+//       this.x += this.speedX;
+//       this.y += this.speedY + this.gravitySpeed;
+//       this.hitBottom(); // stop the falling when it hit bottom area
+//     };
+//     this.hitBottom = function() {
+//       var rockbottom = canvas.height - this.height;
+//       if (this.y > rockbottom) {
+//         this.y = rockbottom;
+//       }
+//     };
+//     ctx.drawImage(this, img, this.x, this.y, this.width, this.height);
+//   }
+// }
+
+// ---------------- Building class for bottom images option----------
+class Option {
+  constructor(img, x) {
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.width = 40;
+    this.height = 40;
+    this.caught = false;
   }
-  if (dir == "down") {
-    myGamePiece.speedY = 1;
-  }
-  if (dir == "left") {
-    myGamePiece.speedX = -1;
-  }
-  if (dir == "right") {
-    myGamePiece.speedX = 1;
+
+  drawOption() {
+    this.x += 3;
+
+    // if the x is past the canvas width
+    if (this.x >= canvas.width) {
+      // reset to negative OPTION width
+      this.x = -this.width - Math.floor(Math.random() * 400);
+    }
+
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
 
-function clearmove() {
-  myGamePiece.image.src = "angrybaby.png";
-  myGamePiece.speedX = 0;
-  myGamePiece.speedY = 0;
-}
-
+// -----------------diaper------------------
 var diaperImg = new Image();
 diaperImg.src = "./img/diaper.png";
-// diaperImg.onload = function() {
-//   // need to check different function to blinking randomly
-//   ctx.drawImage(diaperImg, diaperX, diaperY, 30, 30); //need to check on size again
-// };
 
-function drawDiaper() {
-  ctx.drawImage(diaperImg, diaperX, diaperY, 30, 30);
-}
+var diaper = new Option(diaperImg, 100);
+var diaper2 = new Option(diaperImg, 250);
 
-var diaperX = 100;
-var diaperY = 120;
-
+// ---------------------milk----------------------
 var milkImg = new Image();
 milkImg.src = "./img/milk.png";
-// milkImg.onload = function() {
-//   ctx.drawImage(milkImg, milkX, milkY, 30, 30);
-// };
 
-function drawMilk() {
-  ctx.drawImage(milkImg, milkX, milkY, 30, 30);
-}
+var milk = new Option(milkImg, 350);
+var milk2 = new Option(milkImg, 200);
 
-var milkX = 100;
-var milkY = 200;
-
+//-----------------------handwash----------------------
 var handImg = new Image();
 handImg.src = "./img/handwash.png";
-// diaperImg.onload = function() {
-//   ctx.drawImage(handImg, handX, handY, 30, 30);
-// };
 
-function drawHand() {
-  ctx.drawImage(handImg, handX, handY, 30, 30);
-}
+var hand = new Option(handImg, 500);
+var hand2 = new Option(handImg, 0);
 
-var handX = 100;
-var handY = 400;
+// ------- put bottom images inline (diaper, milk, hand)--------
+var objectsArray = [diaper, diaper2, milk, milk2, hand, hand2];
+var objectParent = [newParent];
 
-//--------- all images --------
+// console.log(objectParent);
 
-var allImages = [drawBaby(), drawDiaper(), drawMilk(), drawHand()];
-// var allImages = [babyImg(), diaperImg(), milkImg(), handImg()];
-
-// console.log(allImages);
 // ---------------------drawing loop ----------------(to make the object move)
 
 drawingLoop();
 function drawingLoop() {
-  ctx.clearRect(0, 0, 600, 500);
+  ctx.clearRect(0, 0, 700, 500);
 
   drawRectangles();
   drawBaby();
-  drawDiaper();
-  drawMilk();
-  drawHand();
+  objectsArray.forEach(function(oneOption) {
+    oneOption.drawOption();
+  });
+  objectParent.forEach(function(oneParent) {
+    console.log(oneParent);
+
+    oneParent.drawParent();
+  });
+
   // drawCreate();
 
   //checkCrashes();
@@ -119,123 +167,75 @@ function drawRectangles() {
 
   ctx.fillStyle = "lightblue";
 
-  ctx.fillRect(0, 0, 600, 500);
+  ctx.fillRect(0, 0, 700, 500);
 
   ctx.strokeStyle = "green";
   ctx.lineWidth = 10;
 
-  ctx.strokeRect(600, 500, 600, 500);
+  ctx.strokeRect(700, 500, 700, 500);
 }
 
 // keyboard controller for baby
 document.onkeydown = function() {
   // console.log("TEST KEY DOWN" + event.keyCode);
   // console.log(event);
-
-  switch (event.keyCode) {
-    case 37: // left arrow (check web for keyboard number > keycode.info)
-      // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
-      event.preventDefault();
-      babyX -= 15;
-      break;
-
-    case 38: // up arrow (check web for keyboard number > keycode.info)
-      // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
-      event.preventDefault();
-      babyY -= 15;
-      break;
-
-    case 39: // right arrow (check web for keyboard number > keycode.info)
-      // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
-      event.preventDefault();
-      babyX += 15;
-      break;
-
-    case 40: // down arrow (check web for keyboard number > keycode.info)
-      // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
-      event.preventDefault();
-      babyY += 15;
-      break;
-  }
+  // switch (event.keyCode) {
+  //   case 37: // left arrow (check web for keyboard number > keycode.info)
+  // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
+  // event.preventDefault();
+  // babyX -= 15;
+  // break;
+  // case 38: // up arrow (check web for keyboard number > keycode.info)
+  // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
+  // event.preventDefault();
+  // babyY -= 15;
+  // break;
+  // case 39: // right arrow (check web for keyboard number > keycode.info)
+  // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
+  // event.preventDefault();
+  // babyX += 15;
+  // break;
+  // case 40: // down arrow (check web for keyboard number > keycode.info)
+  // prevents the default behaviour of keyboard presses (scrolling of ↓↑, screen moving up and down)
+  // event.preventDefault();
+  // babyY += 15;
+  // break;
+  //}
 };
 
 // // ------------------- Collision detection -------------
 
-// function contains(targetA, targetB) {
-//   return !(
-//     targetB.x > targetA.x + targetA.width ||
-//     targetB.x + targetB.width < targetA.x ||
-//     targetB.y > targetA.x + targetA.height ||
-//     targetB.y + targetB.height < targetA.y
+// function rectangleCollision(rectA, rectB) {
+//   console.log(rectA, rectB);
+//   return (
+//     rectA.y + rectA.height >= rectB.y &&
+//     rectA.y <= rectB.y + rectB.height &&
+//     rectA.x + rectA.width >= rectB.x &&
+//     rectA.x <= rectB.x + rectB.width
 //   );
 // }
 
-// // Loop
-// setInterval(onTimerTick, 33);
-
-// // Render Loop
-// function onTimerTick() {
-//   // update object2 to mouse
-//   diaper.x = babyX - baby.width * 0.5;
-//   diaper.y = babyY - baby.height * 0.5;
-
-//   // Clear the canvas
-//   canvas.width = canvas.width;
-
-//   // detect a collision
-//   var collision = contains(baby, diaper);
-// }
-//------different method----
-// var baby = { x: 5, y: 5, width: 70, height: 70 };
-// var diaper = { x: 20, y: 10, width: 30, height: 30 };
-
-// if (
-//   baby.x < diaper.x + diaper.width &&
-//   baby.x + baby.width > diaper.x &&
-//   baby.y < diaper.y + diaper.height &&
-//   baby.y + baby.height > diaper.y
-// ) {
-//   console.log("collision detecteddddd");
+// function checkCrashes() {
+//   console.log(allImages);
+//   allImages.forEach(function(oneImage) {
+//     console.log(oneImage);
+//     if (rectangleCollision(baby, oneImage)) {
+//       console.log(baby);
+//       baby.isCrashed = true;
+//       oneImage.isCrashed = true;
+//     }
+//   });
 // }
 
-// filling in the values =>
-
-//if (5 < 50 && 75 > 20 && 5 < 40 && 75 > 10) {
-// collision detected!
-//}
-
-// --- different method of collision ----
-function rectangleCollision(rectA, rectB) {
-  console.log(rectA, rectB);
-  return (
-    rectA.y + rectA.height >= rectB.y &&
-    rectA.y <= rectB.y + rectB.height &&
-    rectA.x + rectA.width >= rectB.x &&
-    rectA.x <= rectB.x + rectB.width
-  );
-}
-
-function checkCrashes() {
-  console.log(allImages);
-  allImages.forEach(function(oneImage) {
-    console.log(oneImage);
-    if (rectangleCollision(baby, oneImage)) {
-      console.log(baby);
-      baby.isCrashed = true;
-      oneImage.isCrashed = true;
-    }
-  });
-}
-
-class Crash {
-  drawCrash() {
-    if (this.iscrashed) {
-      ctx.fillStyle = "red";
-    } else {
-      ctx.fillStyle = "green";
-    }
-  }
-}
+// class Crash {
+//   drawCrash() {
+//     if (this.iscrashed) {
+//       ctx.fillStyle = "red";
+//     } else {
+//       ctx.fillStyle = "green";
+//     }
+//   }
+// }
 
 // makes the object disappear after get hit by the baby
 
