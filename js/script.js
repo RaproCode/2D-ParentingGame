78cc1a95e638e3ae89b422c45cd06d1cb0b641b5
Raja Prahara <raja.ardri@gmail.com>
2019-01-24 22:51:03 +0100

    working well but still not so beautiful

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var canvas = document.querySelector(".gameFloor");
 // context object to draw things
 var ctx = canvas.getContext("2d");
 
-// -------- AUDIO RESSOURCES
+// --------AUDIO---------
 
 // CREATE Option to catch
 class Option {
@@ -26,13 +26,16 @@ class Option {
       // reset to negative OPTION height
       this.y = -this.height - Math.floor(Math.random() * 400);
       score -= 1; // for score purpose
+      if (score <= 6) {
+        baby.image = babyAngryImg;
+      }
     }
 
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
 
-// CREATE Icecream
+// CREATE Icecream as distraction
 class IceCream {
   constructor(img, x) {
     this.img = img;
@@ -48,7 +51,7 @@ class IceCream {
     }
 
     if (this.y > canvas.height && this.caught === false) {
-      this.y = -this.height - Math.floor(Math.random() * 400);
+      this.y = -this.height - Math.floor(Math.random() * 300);
     }
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
@@ -64,12 +67,13 @@ parentBackImg.src = "./img/ParentTransparentBack.png";
 var babyImg = new Image();
 babyImg.src = "./img/happybaby.png";
 
-// attach the baby to the parent
+var babyAngryImg = new Image();
+babyAngryImg.src = "./img/angrybaby.png";
 
 var diaperImg = new Image();
 diaperImg.src = "./img/diaper.png";
 
-var diaper = new Option(diaperImg, 100);
+var diaper = new Option(diaperImg, 50);
 var diaper2 = new Option(diaperImg, 250);
 
 var milkImg = new Image();
@@ -89,50 +93,17 @@ iceImg.src = "./img/icecream.png";
 
 var ice = new IceCream(iceImg, 300);
 var ice2 = new IceCream(iceImg, 100);
-var ice3 = new IceCream(iceImg, 500);
-
-// var stuffCatch = new Image();
-// letterCatch.src = "./img/.png";
+var ice3 = new IceCream(iceImg, 400);
 
 var gameoverImg = new Image();
 gameoverImg.src = "./img/gameover.jpg";
 
 var winImg = new Image();
-winImg.src = "./img/win.png";
+winImg.src = "./img/win.jpeg";
 
 var objectsArray = [diaper, diaper2, milk, milk2, hand, hand2];
 
 var iceArray = [ice, ice2, ice3];
-
-// //CREATE EXPLOSION SPRITE
-// // var letterHit = {
-// //   x: 500,
-// //   y: 500,
-// //   width: 150,
-// //   height: 150,
-// //   spriteX: 0,
-// //   spriteY: 103,
-// //   image: letterCatch,
-// //   draw() {
-// //     ctx.drawImage(
-// //       letterCatch,
-// //       this.spriteX,
-// //       this.spriteY,
-// //       this.width,
-// //       this.height,
-// //       this.x,
-// //       this.y,
-// //       this.width,
-// //       this.height
-// //     );
-// //     setInterval(function() {
-// //       letterHit.spriteX += 128;
-// //       if (letterHit.spriteX === 640) {
-// //         letterHit.spriteX = 0;
-// //       }
-// //     }, 1000);
-// //   }
-// // };
 
 // CREATE PARENT
 var parent = {
@@ -140,41 +111,23 @@ var parent = {
   y: canvas.height - 100,
   width: 90,
   height: 90,
-  spriteX: 0,
-  spriteY: 450,
   image: parentImg,
-  walkInterval: null,
   flashInterval: null,
   visible: true,
   draw() {
     if (this.visible) {
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        baby.image,
+        this.x + 5,
+        this.y + 5,
+        this.width,
+        this.height
+      );
     }
   },
 
-  stopWalking() {
-    clearInterval(this.walkInterval);
-    this.walkInterval = null;
-    this.x = 0;
-    this.y = 450;
-  },
   // to stop parent loop . stop moving
-  startWalking() {
-    if (this.walkInterval) {
-      return;
-    }
-
-    this.walkInterval = setInterval(function() {
-      parent.x += 150;
-      if (parent.x === 600) {
-        parent.x = 0;
-        parent.y += 150;
-        if (parent.y === 800) {
-          parent.y = 0;
-        }
-      }
-    }, 75);
-  },
 
   parentClick() {
     var count = 0;
@@ -193,137 +146,108 @@ var parent = {
 // CREATE Baby
 var baby = {
   x: 0,
-  y: canvas.height - 235,
+  y: canvas.height - 100,
   width: 40,
   height: 40,
-  image: babyImg,
+  image: babyImg
+  // draw() {
+  //   ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  // }
+};
+
+// CREATE SCORE - ERROR
+var score = 8;
+var radius = 60;
+var zoomInterval = null;
+var red = "#e00d2d";
+var scoreDiv = {
+  flashInterval: null,
+  visible: true,
+
+  // scoreZoom() {
+  //   var radiusList = [
+  //     61,
+  //     62,
+  //     63,
+  //     64,
+  //     65,
+  //     66,
+  //     67,
+  //     68,
+  //     69,
+  //     70,
+  //     70,
+  //     69,
+  //     68,
+  //     67,
+  //     66,
+  //     65,
+  //     64,
+  //     63,
+  //     62,
+  //     61,
+  //     60
+  //   ];
+  //   var count = 0;
+  //   zoomInterval = setInterval(function() {
+  //     radius = radiusList[count];
+  //     count++;
+
+  //     if (count === radiusList.length) {
+  //       clearInterval(zoomInterval);
+  //     }
+  //   }, 50);
+  // },
+
+  scoreClick() {
+    var count = 0;
+
+    clearInterval(this.flashInterval);
+
+    this.flashInterval = setInterval(function() {
+      scoreDiv.visible = !scoreDiv.visible;
+      count++;
+
+      if (count === 6) {
+        clearInterval(scoreDiv.flashInterval);
+        scoreDiv.visible = true;
+      }
+    }, 150);
+  },
+
   draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    if (this.visible) {
+      ctx.beginPath();
+      ctx.arc(canvas.width - 115, 120, radius, 0, 2 * Math.PI);
+      ctx.lineWidth = 6;
+      ctx.stroke();
+      ctx.font = "bold 50px monospace";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(score, canvas.width - 115, 120);
+      //change circle color depending on score
+      if (score >= 15) {
+        ctx.strokeStyle = "#42f462";
+      } else if (score >= 10) {
+        ctx.strokeStyle = "#b5f441";
+      } else if (score > 5) {
+        ctx.strokeStyle = "white";
+      } else {
+        ctx.strokeStyle = red;
+      }
+    }
   }
 };
 
-//CREATE SCORE COUNTER - ERROR
-var score = 5;
-var radius = 60;
-var zoomInterval = null;
-var white = "white";
-
-// var scoreDiv = {
-//   flashInterval: null,
-//   visible: true,
-
-//   scoreZoom() {
-//     var radiusList = [
-//       61,
-//       62,
-//       63,
-//       64,
-//       65,
-//       66,
-//       67,
-//       68,
-//       69,
-//       70,
-//       70,
-//       69,
-//       68,
-//       67,
-//       66,
-//       65,
-//       64,
-//       63,
-//       62,
-//       61,
-//       60
-//     ];
-//     var count = 0;
-//     zoomInterval = setInterval(function() {
-//       radius = radiusList[count];
-//       count++;
-
-//       if (count === radiusList.length) {
-//         clearInterval(zoomInterval);
-//       }
-//     }, 50);
-//   },
-
-//   scoreClick() {
-//     var count = 0;
-//     this.flashInterval = setInterval(function() {
-//       scoreDiv.visible = !scoreDiv.visible;
-//       count++;
-
-//       if (count === 6) {
-//         clearInterval(scoreDiv.flashInterval);
-//         scoreDiv.visible = true;
-//       }
-//     }, 150);
-//   },
-
-//   draw() {
-//     if (this.visible) {
-//       ctx.beginPath();
-//       ctx.arc(canvas.width - 115, 120, radius, 0, 2 * Math.PI);
-//       ctx.lineWidth = 6;
-//       ctx.stroke();
-//       ctx.font = "bold 70px monospace";
-//       ctx.fillStyle = "white";
-//       ctx.textAlign = "center";
-//       ctx.textBaseline = "middle";
-//       ctx.fillText(score, canvas.width - 115, 120);
-//       //change circle color depending on score
-//       if (score > 15) {
-//         ctx.strokeStyle = "#42f462";
-//       } else if (score >= 10) {
-//         ctx.strokeStyle = "#b5f441";
-//       } else if (score > 5) {
-//         ctx.strokeStyle = "white";
-//       } else {
-//         ctx.strokeStyle = red;
-//       }
-
-//       // -----audio depending on score
-// //       if (score <= 0) {
-// //         laughLow.play();
-// //         setTimeout(function() {
-// //           laughLow.pause();
-// //           laughLow.currentTime = 0;
-// //         }, 2000);
-// //       }
-// //       if (score === 10) {
-// //         laugh.play();
-// //         setTimeout(function() {
-// //           laugh.pause();
-// //           laugh.currentTime = 0;
-// //         }, 2000);
-// //       }
-// //       if (score === 15) {
-// //         goodboy.play();
-// //         setTimeout(function() {
-// //           goodboy.pause();
-// //           goodboy.currentTime = 0;
-// //         }, 2500);
-// //       }
-// //       if (score === 20) {
-// //         merry.play();
-// //         setTimeout(function() {
-// //           merry.pause();
-// //           merry.currentTime = 0;
-// //         }, 2000);
-// //       }
-// //       ctx.closePath();
-// //     }
-// //   }
-// // };
-
-// Drawing on Canvas --Erroro
+// Drawing on Canvas
 function drawEverything() {
   ctx.fillStyle = "lightblue"; //background
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#F7F4FA";
   parent.draw();
-  baby.draw();
-  //scoreDiv.draw();
 
+  scoreDiv.draw();
   if (score <= 0) {
     var newWidth = canvas.height * 0.64;
     ctx.drawImage(
@@ -338,7 +262,7 @@ function drawEverything() {
     return;
   }
 
-  if (score >= 15) {
+  if (score >= 20) {
     var newWidth = canvas.height * 0.64;
     ctx.drawImage(
       winImg,
@@ -361,9 +285,13 @@ function drawEverything() {
       score += 1;
       oneOption.caught = true;
 
-      if (score === 10 || score === 15) {
-        scoreDiv.scoreZoom();
+      if (score >= 7) {
+        baby.image = babyImg;
       }
+
+      // if (score === 10 || score === 15) {
+      //   scoreDiv.scoreZoom();
+      // }
       objectsArray.push(
         new Option(oneOption.img, Math.floor(Math.random() * canvas.width))
       ); // A new images from Option is pushed inside [] array so it's never empty.
@@ -381,6 +309,10 @@ function drawEverything() {
       oneIceCream.caught = true;
       parent.parentClick();
       scoreDiv.scoreClick();
+
+      if (score <= 6) {
+        baby.image = babyAngryImg;
+      }
     }
   });
 }
@@ -389,13 +321,13 @@ function drawEverything() {
 function drawingLoop() {
   ctx.clearRect(0, 0, 800, 600);
   drawEverything();
-
   requestAnimationFrame(function() {
     drawingLoop();
   });
 }
 
 drawingLoop();
+// back();
 
 function checkCollision(a, b) {
   return (
@@ -414,10 +346,10 @@ document.onkeydown = function(event) {
       break;
   }
 
-  if (score >= 15 || score <= 0) {
+  if (score >= 20 || score <= 0) {
     return;
   }
-  // parent.startWalking();
+
   switch (event.keyCode) {
     case 37: //<=
       parent.image = parentBackImg;
@@ -430,15 +362,11 @@ document.onkeydown = function(event) {
       break;
   }
 };
-document.onkeyup = function() {
-  // parent.stopWalking();
-};
 
 function restart() {
-  score = 5;
+  score = 8;
   parent.x = 200;
   parent.y = canvas.height - 100;
-  parent.stopWalking();
 
   iceArray = [ice, ice2, ice3];
 
